600ed84d2d2782899cbada579040f9201714bbca
Thomas Boyt <me@thomasboyt.com>
2016-11-21 21:43:33 -0500

    Add friends list to home screen

--- src/client/components/HomeScreenPlaceholder.tsx
+++ src/client/components/HomeScreenPlaceholder.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
+import {Link} from 'react-router';
+import {inject, observer} from 'mobx-react';
+
+import UserStore from '../stores/UserStore';
+
+interface Props {
+  userStore?: UserStore;
+}
 
 // some day this is going to be a cool feed thingy...
-class HomeScreenPlaceholder extends React.Component<{}, {}> {
+@inject((allStores) => ({
+  userStore: allStores.userStore,
+})) @observer
+class HomeScreenPlaceholder extends React.Component<Props, {}> {
   render() {
+    const {following} = this.props.userStore!;
+
     return (
       <div>
         <h2>your buds</h2>
+
+        <ul>
+          {following.map((user) => (
+            <li key={user.id}>
+              <Link to ={`/playlist/${user.twitterName}`}>
+                {user.twitterName}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
