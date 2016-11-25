bc0fb360f18033fbec9ba9b91f89efdca2ce4729
Thomas Boyt <me@thomasboyt.com>
2016-11-25 16:56:14 -0500

    Add user/song/playlist seeds

---
+++ seeds/data/songs.js
@@ -0,0 +1,16 @@
+const songs = [
+  {"id":1,"created_at":null,"updated_at":null,"artists":["Lynyrd Skynyrd"],"album":"Pronounced' Leh-'Nerd 'Skin-'Nerd","title":"Free Bird","spotify_id":"5EWPGh7jbTNO2wakv8LjUI","album_art":"https://i.scdn.co/image/a30e6162907f82794bd32351ca8a8e0c6b91d8f5"},
+  {"id":2,"created_at":null,"updated_at":null,"artists":["The Chemical Brothers"],"album":"Brotherhood","title":"Block Rockin' Beats - 2003 Digital Remaster","spotify_id":"2wPFy7SAFnt9Nj2TipWcqb","album_art":"https://i.scdn.co/image/c48d3e3e9b0883b598cedbe36db912f7f1137a1e"},
+  {"id":3,"created_at":null,"updated_at":null,"artists":["Incubus"],"album":"Make Yourself","title":"Drive","spotify_id":"7nnWIPM5hwE3DaUBkvOIpy","album_art":"https://i.scdn.co/image/c002bd0cd798e8cb5873e3513a987287c2993bea"},
+];
+
+const entries = [
+  {"id":1,"created_at":null,"updated_at":null,"song_id":1,"user_id":2,"youtube_url":"https://www.youtube.com/watch?v=D0W1v0kOELA"},
+  {"id":2,"created_at":null,"updated_at":null,"song_id":2,"user_id":1,"youtube_url":"https://www.youtube.com/watch?v=A0kluTStX_Y"},
+  {"id":3,"created_at":null,"updated_at":null,"song_id":3,"user_id":4,"youtube_url":"https://www.youtube.com/watch?v=fgT9zGkiLig"}
+];
+
+module.exports = {
+  songs: songs,
+  entries: entries,
+};
--- seeds/users.js
+++ seeds/users.js
@@ -1,17 +1,47 @@
+const songData = require('./data/songs');
+
+function createUser(knex, name, userId) {
+  return knex('users').insert({
+    id: userId,
+    auth_token: name,
+    twitter_name: name,
+    twitter_id: `${userId}`,
+    twitter_token: `${userId}`,
+    twitter_secret: `${userId}`,
+  });
+}
+
+function followUser(knex, userId, followingId) {
+  return knex('following').insert({
+    user_id: userId,
+    following_id: followingId,
+  });
+}
+
+function createSong(knex, song) {
+  return knex('songs').insert(song);
+}
+
+function createEntry(knex, entry) {
+  return knex('playlist_entries').insert(entry);
+}
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('users').insert({
-          auth_token: 'devaccount',
-          twitter_name: 'devaccount',
-          twitter_id: '1234',
-          twitter_token: '1234',
-          twitter_secret: '1234',
-        }),
-      ]);
-    });
+  return Promise.all([
+    createUser(knex, 'jeff', 1),
+    createUser(knex, 'dan', 2),
+    createUser(knex, 'brad', 3),
+    createUser(knex, 'vinny', 4),
+
+  ]).then(() => {
+    // jeff follows dan and vinny
+    return Promise.all([
+      followUser(knex, 1, 2),
+      followUser(knex, 1, 4),
+    ]);
+  }).then(() => {
+    return Promise.all(songData.songs.map((s) => createSong(knex, s)));
+  }).then(() => {
+    return Promise.all(songData.entries.map((e) => createEntry(knex, e)));
+  });
 };
