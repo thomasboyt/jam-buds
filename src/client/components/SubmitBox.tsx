612cdbed83a34ac8e4e1963203b737a2108367d0
Thomas Boyt <me@thomasboyt.com>
2016-11-27 16:21:11 -0500

    Add frontend validation to Youtube URLs

--- src/client/components/SubmitBox.tsx
+++ src/client/components/SubmitBox.tsx
@@ -4,8 +4,16 @@ import {observer, inject} from 'mobx-react';
 import AddSongStore from '../stores/AddSongStore';
 import getSongsSearch from '../api/getSongsSearch';
 
+import * as classNames from 'classnames';
+
 interface Props {
   addSongStore?: AddSongStore
+}
+
+const YOUTUBE_RE = /https:\/\/www\.youtube\.com\/watch\?v=(.*)/;
+
+function validYoutubeAddress(url: string): boolean {
+  return YOUTUBE_RE.test(url);
 }
 
 @inject((allStores) => ({
@@ -16,21 +24,31 @@ class SubmitBox extends React.Component<Props, {}> {
 
   state = {
     value: '',
+    invalid: false,
   };
 
   handleChange(e: React.SyntheticEvent<HTMLInputElement>) {
     this.setState({
       value: e.currentTarget.value,
+      invalid: false,
     });
   }
 
   handleSubmit(e: React.FormEvent<any>) {
     e.preventDefault();
 
-    const query = this.state.value;
+    const url = this.state.value;
+
+    if (!validYoutubeAddress(url)) {
+      this.setState({
+        invalid: true,
+      });
+
+      return;
+    }
 
     const {addSongStore} = this.props;
-    addSongStore!.showAddSongScreen(query);
+    addSongStore!.showAddSongScreen(url);
 
     this.setState({
       value: '',
@@ -38,16 +56,25 @@ class SubmitBox extends React.Component<Props, {}> {
   }
 
   render() {
+    const {invalid} = this.state;
+    const inputClassName = classNames({
+      invalid,
+    });
+
+    const buttonDisabled = this.state.value === '' || this.state.invalid;
+
     return (
       <form className="submit-box" onSubmit={(e) => this.handleSubmit(e)}>
+
         <div className="input-container">
           <input type="text"
+            className={inputClassName}
             onChange={(e) => this.handleChange(e)}
             value={this.state.value}
             placeholder="Paste a Youtube link here!!" />
         </div>
 
-        <button type="submit" disabled={this.state.value === ''}>
+        <button type="submit" disabled={buttonDisabled} className={inputClassName}>
           <span>is my shit</span>
         </button>
       </form>
