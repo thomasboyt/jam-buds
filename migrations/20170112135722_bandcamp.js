d2c573f24bbecc00c347aaf459a6cfc49755a3aa
Thomas Boyt <me@thomasboyt.com>
2017-01-17 17:02:38 -0500

    Backfill source column for existing playlist entries

--- migrations/20170112135722_bandcamp.js
+++ migrations/20170112135722_bandcamp.js
@@ -1,11 +1,15 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.alterTable('playlist_entries', (table) => {
-    table.enum('source', ['youtube', 'bandcamp']).notNullable();
+    table.enum('source', ['youtube', 'bandcamp']); //.notNullable();
 
     table.string('bandcamp_track_id').nullable();
     table.string('bandcamp_url').nullable();
     table.string('bandcamp_streaming_url').nullable();
+  }).then(() => {
+    return knex('playlist_entries').update({'source': 'youtube'});
+  }).then(() => {
+    return knex.raw('ALTER TABLE playlist_entries ALTER COLUMN source SET NOT NULL;');
   });
 
 };
