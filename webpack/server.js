e5cf55403f2d4a08bd1dfd0ab3a533110c02cfce
Thomas Boyt <me@thomasboyt.com>
2017-04-20 01:57:49 -0400

    Render server through Webpack

---
+++ webpack/server.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+var webpack = require('webpack');
+
+var nodeModules = {};
+
+/*
+ * Prevent bundling of node modules
+ */
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: {
+    server: './src/server/entry.ts',
+  },
+
+  target: 'node',
+
+  output: {
+    path: './build/server',
+    filename: '[name].bundle.js'
+  },
+
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.tsx', '.ts'],
+
+    alias: {
+      '__root': process.cwd(),
+    },
+  },
+
+  devtool: 'source-map',
+
+  externals: nodeModules,
+
+  plugins: [
+    new webpack.BannerPlugin('require("source-map-support").install();',
+                             { raw: true, entryOnly: false })
+  ],
+
+  ts: {
+    compilerOptions: {
+      noEmit: false,
+    },
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts',
+      },
+
+      {
+        test: /\.jpg$|\.png$/,
+        loader: 'file',
+      },
+
+      {
+        test: /\.svg$/,
+        loader: 'raw',
+      }
+    ]
+  }
+};
