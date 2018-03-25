34a6e808197a76c36788f099f996dbada32cbde2
Thomas Boyt <me@thomasboyt.com>
2018-03-25 09:54:23 -0400

    api: Add twitter_name -> name migration

---
+++ api/migrations/20180325094513_username_from_twittername.js
@@ -0,0 +1,19 @@
+exports.up = async function(knex, Promise) {
+  await knex.schema.alterTable('users', (table) => {
+    table.string('name').nullable(); // make nullable initially
+  });
+
+  await knex('users')
+    .select()
+    .map(async (row) => {
+      await knex('users')
+        .where({ id: row.id })
+        .update({ name: row.twitter_name });
+    });
+
+  await knex.raw('ALTER TABLE users ALTER COLUMN name SET NOT NULL;');
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.resolve();
+};
