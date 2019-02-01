d65322172dbe3f2d0e69f5df823a07a555029248
Thomas Boyt <me@thomasboyt.com>
2019-02-01 04:14:21 -0500

    app: Add email sign-in/registration form

--- app/src/components/LoggedOutHome.vue
+++ app/src/components/LoggedOutHome.vue
@@ -3,27 +3,36 @@
     <h2>hey! listen!</h2>
 
     <div class="lower">
-      <p>this is <strong>jam buds</strong>, a site to share music with friends</p>
-
       <p>
-        you can post songs you like, follow your friends, and listen to what everyone you know is listening to.
+        this is
+        <strong>jam buds</strong>, a site to share music with friends
       </p>
+
+      <p>you can post songs you like, follow your friends, and listen to what everyone you know is listening to.</p>
 
       <div class="twitter-auth">
         <p>want to get started?</p>
 
-        <a class="button" href="/auth/twitter-sign-in">
-          Sign in with Twitter
-        </a>
+        <sign-in-form/>
       </div>
     </div>
 
     <div class="attribution">
-      site by <a href="https://twitter.com/thomasABoyt">thomas</a>
+      site by
+      <a href="https://twitter.com/thomasABoyt">thomas</a>
       &middot;
-      good doggo by <a href="http://twitter.com/rainbowfission">@rainbowfission</a>
+      good doggo by
+      <a href="http://twitter.com/rainbowfission">@rainbowfission</a>
       &middot;
       <router-link to="/about">about jam buds</router-link>
     </div>
   </div>
 </template>
+
+<script>
+import SignInForm from './SignInForm.vue';
+
+export default {
+  components: { SignInForm },
+};
+</script>
---
+++ app/src/components/SignInForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit="handleSubmit">
+    <input type="email" v-model="email" placeholder="enter email...">
+    
+    <button type="submit">get started</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+import apiRequest from '../apiRequest';
+
+export default {
+  data() {
+    return {
+      email: '',
+    };
+  },
+
+  methods: {
+    async handleSubmit(evt) {
+      evt.preventDefault();
+
+      const email = this.email;
+
+      const resp = await axios.post(
+        `${process.env.API_URL}/auth/sign-in-token`,
+        {
+          email,
+        }
+      );
+
+      if (resp.status === 200) {
+        console.log('token generated, check log');
+      }
+    },
+  },
+};
+</script>
