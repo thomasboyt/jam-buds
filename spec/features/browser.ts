8ba4d2ae046cadc3957632d2bc9657519d64b60b
Thomas Boyt <me@thomasboyt.com>
2019-02-11 22:53:37 -0500

    Spawn dev server from feature tests if not started

--- spec/features/browser.ts
+++ spec/features/browser.ts
@@ -3,7 +3,9 @@ import puppeteer from 'puppeteer';
 export let browser: puppeteer.Browser;
 
 before(async () => {
-  browser = await puppeteer.launch();
+  browser = await puppeteer.launch({
+    // headless: false,
+  });
 });
 
 after(async () => {
--- spec/features/mochaHooks.ts
+++ spec/features/mochaHooks.ts
@@ -3,12 +3,25 @@ import path from 'path';
 import childProcess from 'child_process';
 import dotenv from 'dotenv';
 import waitPort from 'wait-port';
+import got from 'got';
 
 if (!process.env.CI) {
   dotenv.config({
     path: '../.env.test',
   });
 }
+
+const requiredEnv = ['DATABASE_URL', 'APP_URL', 'API_URL', 'STATIC_URL'];
+
+for (let key of requiredEnv) {
+  if (!process.env[key]) {
+    throw new Error(`missing required env var ${key}`);
+  }
+}
+
+// TODO: parse this from API_URL and APP_URL
+const API_PORT = 3001;
+const APP_PORT = 8080;
 
 async function resetDb() {
   const db = knex({
@@ -34,37 +47,71 @@ async function resetDb() {
   }
 }
 
-function waitForStart(ms: number): Promise<any> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(), ms);
+async function startApiServer() {
+  console.log('Starting API server');
+
+  const apiServer = childProcess.spawn('npm', ['start'], {
+    cwd: path.join(__dirname, '../../api'),
+    stdio: 'inherit',
+    env: {
+      ...process.env,
+      PORT: API_PORT,
+    },
   });
+
+  process.on('exit', () => {
+    apiServer.kill();
+  });
+
+  const started = await waitPort({
+    port: API_PORT,
+    timeout: 10 * 1000,
+  });
+
+  if (!started) {
+    throw new Error('api server timed out :(');
+  }
+}
+
+async function startAppServer() {
+  try {
+    // If this doesn't throw an error, the server's already running!
+    await got(process.env.APP_URL!);
+    return;
+  } catch {
+    // continue
+  }
+
+  const appServer = childProcess.spawn('npm', ['start'], {
+    cwd: path.join(__dirname, '../../app'),
+    stdio: 'inherit',
+    env: {
+      ...process.env,
+      PORT: APP_PORT,
+    },
+  });
+
+  process.on('exit', () => {
+    appServer.kill();
+  });
+
+  const appStarted = await waitPort({
+    port: APP_PORT,
+    timeout: 10 * 1000,
+  });
+
+  if (!appStarted) {
+    throw new Error('app server timed out :(');
+  }
+
+  console.log('Waiting for app server...');
+  await got(process.env.APP_URL!);
 }
 
 before(async function() {
   this.timeout(30 * 1000);
 
-  console.log('Starting API server');
-
-  // this uses a node entry point instead of ts-node cuz i couldn't easily clean
-  // up after ts-node exited
-  const child = childProcess.spawn('npm', ['start'], {
-    cwd: path.join(__dirname, '../../api'),
-    stdio: 'inherit',
-  });
-
-  process.on('exit', () => {
-    child.kill();
-  });
-
-  console.log('Waiting for API server start');
-  const started = await waitPort({
-    port: parseInt(process.env.PORT!),
-    timeout: 10 * 1000,
-  });
-
-  if (!started) {
-    throw new Error('timed out :(');
-  }
+  await Promise.all([startApiServer(), startAppServer()]);
 });
 
 beforeEach(async () => {
