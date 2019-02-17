538247d7cb044daece3fcfdbf080e4ff11e9d154
Thomas Boyt <me@thomasboyt.com>
2019-02-17 17:17:44 -0500

    API: Skip Apple Music lookup if DISABLE_APPLE_MUSIC set or in testing

--- api/src/apis/appleMusic.ts
+++ api/src/apis/appleMusic.ts
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import generateMusicKitToken from '@tboyt/music-kit-jwt';
 
-let token: string;
+let cachedToken: string;
+function getToken(): string {
+  if (cachedToken) {
+    return cachedToken;
+  }
 
-// skip token generation in unit tests
-if (process.env.NODE_ENV !== 'test' || process.env.TEST_ENV === 'feature') {
-  token = generateMusicKitToken({
+  cachedToken = generateMusicKitToken({
     pathToPrivateKey: process.env.MUSICKIT_PRIVATE_KEY_PATH!,
     keyId: process.env.MUSICKIT_KEY_ID!,
     teamId: process.env.MUSICKIT_TEAM_ID!,
   });
+
+  return cachedToken;
 }
 
 /**
@@ -18,6 +22,12 @@ if (process.env.NODE_ENV !== 'test' || process.env.TEST_ENV === 'feature') {
 export async function getAppleMusicIDByISRC(
   isrc: string
 ): Promise<string | null> {
+  if (process.env.NODE_ENV === 'test' || process.env.DISABLE_APPLE_MUSIC) {
+    return null;
+  }
+
+  const token = getToken();
+
   const searchUrl = `https://api.music.apple.com/v1/catalog/us/songs?filter[isrc]=${isrc}`;
 
   const resp = await axios.get(searchUrl, {
