3e4f0f7fd3023109652a44a6969b659dc256528d
Thomas Boyt <me@thomasboyt.com>
2019-02-18 20:22:49 -0500

    App: Remove leftover console.log

--- app/src/configureSentry.js
+++ app/src/configureSentry.js
@@ -2,7 +2,6 @@ import * as Sentry from '@sentry/browser';
 import Vue from 'vue';
 
 if (process.env.SENTRY_PUBLIC_DSN_APP) {
-  console.log('initializing sentry!');
   Sentry.init({
     dsn: process.env.SENTRY_PUBLIC_DSN_APP,
     integrations: [
