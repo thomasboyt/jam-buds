5d92634401ab84121c06882c22ff9135ec975bd7
Thomas Boyt <thomas@workbravely.com>
2019-02-21 12:43:42 -0500

    Add docker-compose configs for dev & prod

--- ops/remote-tasks/restart-docker.sh
+++ ops/remote-tasks/restart-docker.sh
@@ -5,4 +5,4 @@ trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 
 cd $JAMBUDS_REMOTE_PATH
 
-docker-compose up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
