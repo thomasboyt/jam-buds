bde7d52c6420fe77a96542bbc2b6e0afd43a2c35
Thomas Boyt <me@thomasboyt.com>
2019-02-21 01:08:01 -0500

    App: add share banner to top of user profiles for logged out users

--- app/src/components/ProfileNav.vue
+++ app/src/components/ProfileNav.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="user-header">
+    <template v-if="!authenticated">
+      <div class="share-banner">
+        <div>
+          HEY! this is jam buds, a site that lets you share music with your
+          pals!
+        </div>
+        <div>
+          <router-link :to="`/?signup-ref=${name}`">sign up</router-link> to
+          follow what <strong>{{ name }}</strong> is listening to, and share
+          your own jams!
+        </div>
+      </div>
+    </template>
+
     <div class="user-header-top">
       <h2>
         {{ title }}
@@ -54,6 +68,7 @@ export default {
   computed: {
     ...mapState({
       name: (state) => state.profile.user.name,
+      authenticated: (state) => state.auth.authenticated,
       showFollowToggle: (state) =>
         state.auth.authenticated &&
         state.profile.user.name !== state.currentUser.name,
@@ -61,3 +76,17 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.share-banner {
+  font-size: 18px;
+  line-height: 24px;
+  border: 1px var(--theme-text-color) solid;
+  color: var(--theme-text-color);
+  box-shadow: 5px 5px 0px var(--theme-text-color);
+  padding: 20px 10px;
+  border-radius: 4px;
+  margin-bottom: 30px;
+  text-align: center;
+}
+</style>
