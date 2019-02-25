bebf5ee4f740dae74af2e2a7459728d18534baf1
Thomas Boyt <thomas@workbravely.com>
2019-02-25 13:20:01 -0500

    API: Create test database during setup

--- api/src/__tests__/index.spec.ts
+++ api/src/__tests__/index.spec.ts
@@ -1,3 +1,5 @@
+import { Client } from 'pg';
+import { parse } from 'pg-connection-string';
 import dotenv from 'dotenv';
 
 // (this is only used for local testing, and doesn't exist in CI)
@@ -9,7 +11,31 @@ dotenv.config({
 import { db, configureDatabase } from '../db';
 import { configureRedis, redis } from '../redis';
 
+async function createTestDatabase(): Promise<void> {
+  // create db if it doesn't already exist
+  const connection = parse(process.env.DATABASE_URL!) as any;
+  const client = new Client({
+    ...connection,
+    database: 'postgres',
+  });
+
+  await client.connect();
+  const { rowCount } = await client.query(
+    'SELECT 1 FROM pg_database WHERE datname = $1',
+    [connection.database]
+  );
+  const dbExists = rowCount === 1;
+
+  if (!dbExists) {
+    await client.query(`CREATE DATABASE ${connection.database}`);
+  }
+
+  await client.end();
+}
+
 before(async () => {
+  await createTestDatabase();
+
   configureDatabase();
   configureRedis();
 
