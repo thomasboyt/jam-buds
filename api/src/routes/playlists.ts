6ee6dbc9d0964f664d75ad34e1d61d524925b5a0
Thomas Boyt <me@thomasboyt.com>
2020-01-06 20:30:13 -0500

    API: Unify playlist route logic

--- api/src/routes/playlists.ts
+++ api/src/routes/playlists.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request } from 'express';
 
 import {
   UserModel,
@@ -17,6 +17,39 @@ import { getUserFromRequest, isAuthenticated } from '../auth';
 import wrapAsyncRoute from '../util/wrapAsyncRoute';
 import { ENTRY_PAGE_LIMIT } from '../constants';
 import { UserPlaylist, Playlist } from '../resources';
+import { JamBudsHTTPError } from '../util/errors';
+
+async function getUser(userName: string): Promise<UserModel> {
+  const user = await getUserByName(userName);
+
+  if (!user) {
+    throw new JamBudsHTTPError({
+      statusCode: 404,
+      message: `No user found with name ${userName}`,
+    });
+  }
+
+  return user;
+}
+
+async function getPlaylistQueryOptions(
+  req: Request,
+  currentUser?: UserModel | null
+) {
+  if (!currentUser) {
+    currentUser = await getUserFromRequest(req);
+  }
+
+  // TODO: validate these
+  const beforeTimestamp = req.query.before;
+  const afterTimestamp = req.query.after;
+
+  return {
+    currentUserId: currentUser ? currentUser.id : undefined,
+    beforeTimestamp,
+    afterTimestamp,
+  };
+}
 
 export default function registerPlaylistEndpoints(router: Router) {
   // get a user's playlist
@@ -24,25 +57,10 @@ export default function registerPlaylistEndpoints(router: Router) {
     '/playlists/:userName',
     wrapAsyncRoute(async (req, res) => {
       const userName = req.params.userName;
-      const user = await getUserByName(userName);
-      const currentUser = await getUserFromRequest(req);
+      const user = await getUser(userName);
+      const queryOptions = await getPlaylistQueryOptions(req);
 
-      if (!user) {
-        res.status(404).json({
-          error: `No user found with name ${userName}`,
-        });
-
-        return;
-      }
-
-      const beforeTimestamp = req.query.before;
-      const afterTimestamp = req.query.after;
-
-      const items = await getPostsByUserId(user.id, {
-        currentUserId: currentUser ? currentUser.id : undefined,
-        beforeTimestamp,
-        afterTimestamp,
-      });
+      const items = await getPostsByUserId(user.id, queryOptions);
 
       const resp: UserPlaylist = {
         userProfile: await getUserProfileForUser(user),
@@ -59,25 +77,30 @@ export default function registerPlaylistEndpoints(router: Router) {
     '/playlists/:userName/liked',
     wrapAsyncRoute(async (req, res) => {
       const userName = req.params.userName;
-      const user = await getUserByName(userName);
-      const currentUser = await getUserFromRequest(req);
+      const user = await getUser(userName);
+      const queryOptions = await getPlaylistQueryOptions(req);
 
-      if (!user) {
-        res.status(404).json({
-          error: `No user found with name ${userName}`,
-        });
+      const items = await getLikesByUserId(user.id, queryOptions);
 
-        return;
-      }
+      const resp: UserPlaylist = {
+        userProfile: await getUserProfileForUser(user),
+        items,
+        limit: ENTRY_PAGE_LIMIT,
+      };
 
-      const beforeTimestamp = req.query.before;
-      const afterTimestamp = req.query.after;
+      res.json(resp);
+    })
+  );
 
-      const items = await getLikesByUserId(user.id, {
-        currentUserId: currentUser ? currentUser.id : undefined,
-        beforeTimestamp,
-        afterTimestamp,
-      });
+  // Get mixtapes for a specific user
+  router.get(
+    '/users/:userName/mixtapes',
+    wrapAsyncRoute(async (req, res) => {
+      const userName = req.params.userName;
+      const user = await getUser(userName);
+      const queryOptions = await getPlaylistQueryOptions(req);
+
+      const items = await getPublishedMixtapesByUserId(user.id, queryOptions);
 
       const resp: UserPlaylist = {
         userProfile: await getUserProfileForUser(user),
@@ -93,17 +116,10 @@ export default function registerPlaylistEndpoints(router: Router) {
     '/feed',
     isAuthenticated,
     wrapAsyncRoute(async (req, res) => {
-      const user: UserModel = res.locals.user;
+      const currentUser: UserModel = res.locals.user;
+      const queryOptions = await getPlaylistQueryOptions(req, currentUser);
 
-      // TODO: validate
-      const beforeTimestamp = req.query.before;
-      const afterTimestamp = req.query.after;
-
-      const items = await getFeedByUserId(user.id, {
-        currentUserId: user.id,
-        beforeTimestamp,
-        afterTimestamp,
-      });
+      const items = await getFeedByUserId(currentUser.id, queryOptions);
 
       const feed: Playlist = {
         items,
@@ -117,17 +133,9 @@ export default function registerPlaylistEndpoints(router: Router) {
   router.get(
     '/public-feed',
     wrapAsyncRoute(async (req, res) => {
-      const currentUser = await getUserFromRequest(req);
+      const queryOptions = await getPlaylistQueryOptions(req);
 
-      // TODO: validate
-      const beforeTimestamp = req.query.before;
-      const afterTimestamp = req.query.after;
-
-      const items = await getPublicFeed({
-        currentUserId: currentUser ? currentUser.id : undefined,
-        beforeTimestamp,
-        afterTimestamp,
-      });
+      const items = await getPublicFeed(queryOptions);
 
       const feed: Playlist = {
         items,
@@ -135,41 +143,6 @@ export default function registerPlaylistEndpoints(router: Router) {
       };
 
       res.json(feed);
-    })
-  );
-
-  // Get mixtapes for a specific user
-  router.get(
-    '/users/:userName/mixtapes',
-    wrapAsyncRoute(async (req, res) => {
-      const userName = req.params.userName;
-      const user = await getUserByName(userName);
-      const currentUser = await getUserFromRequest(req);
-
-      if (!user) {
-        res.status(400).json({
-          error: `Could not find user with name ${userName}`,
-        });
-
-        return;
-      }
-
-      const beforeTimestamp = req.query.before;
-      const afterTimestamp = req.query.after;
-
-      const items = await getPublishedMixtapesByUserId(user.id, {
-        currentUserId: currentUser ? currentUser.id : undefined,
-        beforeTimestamp,
-        afterTimestamp,
-      });
-
-      const resp: UserPlaylist = {
-        userProfile: await getUserProfileForUser(user),
-        items,
-        limit: ENTRY_PAGE_LIMIT,
-      };
-
-      res.json(resp);
     })
   );
 }
