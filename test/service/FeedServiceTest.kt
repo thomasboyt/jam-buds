cc7f7278cc8637e2ec4126c3ca544f8a0d3f2b91
Thomas Boyt <thomas.boyt@classpass.com>
2020-01-21 19:43:12 -0500

    Rhiannon: WIP: wrap tests in transactions

--- test/service/FeedServiceTest.kt
+++ test/service/FeedServiceTest.kt
@@ -1,8 +1,9 @@
 package service
 
 import org.flywaydb.core.Flyway
-import org.jdbi.v3.sqlobject.kotlin.onDemand
 import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.Handle
+import org.jdbi.v3.core.kotlin.useTransactionUnchecked
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
 import kotlin.test.assertEquals
@@ -11,22 +12,43 @@ import createJdbi
 class FeedServiceTest {
     private lateinit var jdbi: Jdbi
 
-    @BeforeEach()
+    @BeforeEach
     fun beforeEach() {
-        // TODO: would be nice to create the DB if it doesn't exist here....
         val flyway = Flyway.configure()
             .dataSource("jdbc:postgresql://localhost:5433/jambuds_kotlin_test", "postgres", "")
             .load()
+
+        // TODO: figure out how to only clean if first test run here
         flyway.clean()
+
         flyway.migrate()
         jdbi = createJdbi("jdbc:postgresql://localhost:5433/jambuds_kotlin_test?user=postgres")
     }
 
     @Test
     fun `getPublicFeed - returns an empty list with no items`() {
-        val feedService = createFeedService()
-        val results = feedService.getPublicFeed(beforeTimestamp = null, afterTimestamp = null)
-        assertEquals(0, results.size)
+        withTransaction { txn ->
+            val feedService = createFeedService(txn)
+            val results = feedService.getPublicFeed(beforeTimestamp = null, afterTimestamp = null)
+            assertEquals(0, results.size)
+        }
+    }
+
+    @Test
+    fun `getPublicFeed - returns public feed entries`() {
+        withTransaction { txn ->
+            val feedService = createFeedService(txn)
+
+            txn.createUpdate("""
+                insert into users (name, show_in_public_feed) values (:name, :show_in_public_feed)
+                """.trimIndent())
+                .bind("name", "thomas")
+                .bind("show_in_public_feed", true)
+                .execute()
+
+            val results = feedService.getPublicFeed(beforeTimestamp = null, afterTimestamp = null)
+            assertEquals(3, results.size)
+        }
     }
 
 //    @Test
@@ -37,10 +59,19 @@ class FeedServiceTest {
 //    fun `getPublicFeed - does not apply a limit when using an after timestamp`() {
 //    }
 
-    private fun createFeedService(): FeedService {
-        val postDao = jdbi.onDemand<dao.PostDao>()
-        val songDao = jdbi.onDemand<dao.SongDao>()
-        val mixtapeDao = jdbi.onDemand<dao.MixtapeDao>()
+    private fun withTransaction(cb: (txn: Handle) -> Unit) {
+        jdbi.useTransactionUnchecked {
+            cb(it)
+            // if the test errors out, it will automatically execute a rollback; we only need a
+            // manual rollback if we made it this far
+            it.rollback()
+        }
+    }
+
+    private fun createFeedService(txn: Handle): FeedService {
+        val postDao = txn.attach(dao.PostDao::class.java)
+        val songDao = txn.attach(dao.SongDao::class.java)
+        val mixtapeDao = txn.attach(dao.MixtapeDao::class.java)
         return FeedService(postDao, songDao, mixtapeDao)
     }
 }
