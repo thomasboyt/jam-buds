e892384c09f96640bd3d75a3639f8e48a01c6ff6
Thomas Boyt <me@thomasboyt.com>
2020-03-03 20:43:13 -0500

    CI: Fix API unit tests

--- api/src/__tests__/index.spec.ts
+++ api/src/__tests__/index.spec.ts
@@ -7,9 +7,16 @@ import { configureRedis } from '../redis';
 
 before(async function() {
   this.timeout(15000);
+
   execSync(path.resolve(__dirname, '../../../spec/bin/create_schema.sh'), {
     stdio: 'inherit',
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      JDBC_DATABASE_URL: process.env.JDBC_DATABASE_URL,
+      REDIS_URL: process.env.REDIS_URL,
+    },
   });
+
   configureDatabase();
   configureRedis();
 });
