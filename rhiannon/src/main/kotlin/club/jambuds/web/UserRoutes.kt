9803b5c66d4c352ac3207ef5cbabdc48e6a00dd8
Thomas Boyt <me@thomasboyt.com>
2020-04-17 17:41:08 -0400

    Rhiannon: Port over GET following/followers endpoints

--- rhiannon/src/main/kotlin/club/jambuds/web/UserRoutes.kt
+++ rhiannon/src/main/kotlin/club/jambuds/web/UserRoutes.kt
@@ -2,16 +2,20 @@ package club.jambuds.web
 
 import club.jambuds.responses.GetCurrentUserResponse
 import club.jambuds.responses.TwitterFriendSuggestionsResponse
+import club.jambuds.responses.UserFollowingResponse
 import club.jambuds.service.UserService
 import club.jambuds.web.extensions.currentUser
 import club.jambuds.web.extensions.requireUser
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.http.Context
+import io.javalin.http.NotFoundResponse
 
 class UserRoutes(private val userService: UserService) {
     fun register() {
         get("/api/me", this::getCurrentUser)
         get("/api/friend-suggestions", this::getTwitterFriendSuggestions)
+        get("/api/users/:userName/following", this::getUserFollowing)
+        get("/api/users/:userName/followers", this::getUserFollowers)
     }
 
     private fun getCurrentUser(ctx: Context) {
@@ -31,5 +35,25 @@ class UserRoutes(private val userService: UserService) {
         val user = ctx.requireUser()
         val users = userService.getUnfollowedTwitterUsersForUser(user)
         ctx.json(TwitterFriendSuggestionsResponse(users))
+    }
+
+    private fun getUserFollowing(ctx: Context) {
+        val userName = ctx.pathParam<String>("userName").get()
+        val userProfile = userService.getUserProfileByName(userName)
+            ?: throw NotFoundResponse("User not found with name $userName")
+
+        val users = userService.getFollowingByUserId(userProfile.id)
+
+        ctx.json(UserFollowingResponse(userProfile, users))
+    }
+
+    private fun getUserFollowers(ctx: Context) {
+        val userName = ctx.pathParam<String>("userName").get()
+        val userProfile = userService.getUserProfileByName(userName)
+            ?: throw NotFoundResponse("User not found with name $userName")
+
+        val users = userService.getFollowersByUserId(userProfile.id)
+
+        ctx.json(UserFollowingResponse(userProfile, users))
     }
 }
