3a6b9454b11b37046647569c572181c0cfaff2fc
Thomas Boyt <me@thomasboyt.com>
2020-08-31 22:24:06 -0400

    App: Move <head> config out of config into root layout

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -39,41 +39,6 @@ export default {
   /*
    ** Headers of the page
    */
-  head: {
-    titleTemplate: (title) => (title ? `${title} | Jam Buds` : 'Jam Buds'),
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-
-      { name: 'twitter:card', content: 'summary' },
-      { hid: 'title', name: 'og:title', content: 'jam buds!' },
-      {
-        hid: 'description',
-        name: 'og:description',
-        content: 'a place for sharing music with your friends!',
-      },
-
-      {
-        name: 'og:image',
-        content: `${process.env.JB_STATIC_URL}/corgi_icon_square.png`,
-      },
-    ],
-
-    link: [
-      {
-        rel: 'icon',
-        type: 'image/png',
-        href: `${process.env.JB_STATIC_URL}/favicon16.png`,
-        sizes: '16x16',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        href: `${process.env.JB_STATIC_URL}/favicon32.png`,
-        sizes: '32x32',
-      },
-    ],
-  },
 
   router: {
     extendRoutes(routes, resolve) {
--- app/src/layouts/default.vue
+++ app/src/layouts/default.vue
@@ -39,6 +39,44 @@ export default {
     FlashMessage,
   },
 
+  head() {
+    return {
+      titleTemplate: (title) => (title ? `${title} | Jam Buds` : 'Jam Buds'),
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+        { name: 'twitter:card', content: 'summary' },
+        { hid: 'title', name: 'og:title', content: 'jam buds!' },
+        {
+          hid: 'description',
+          name: 'og:description',
+          content: 'a place for sharing music with your friends!',
+        },
+
+        {
+          name: 'og:image',
+          content: `${this.$config.STATIC_URL}/corgi_icon_square.png`,
+        },
+      ],
+
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: `${this.$config.STATIC_URL}/favicon16.png`,
+          sizes: '16x16',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: `${this.$config.STATIC_URL}/favicon32.png`,
+          sizes: '32x32',
+        },
+      ],
+    };
+  },
+
   computed: mapState({
     authenticated: (state) => state.auth.authenticated,
     addSongModalIsOpen: (state) => state.addSong.showModal,
