e86cdc40831c682a50e064f4a1383091e32a9f92
Thomas Boyt <me@thomasboyt.com>
2020-09-19 12:56:36 -0400

    App: Hide Android tap-highlights when in web view

--- app/src/layouts/default.vue
+++ app/src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-container">
+  <div :class="['app-container', { 'webview-container': isWebView }]">
     <mobile-header />
     <mobile-bottom-tabs v-if="authenticated" />
 
