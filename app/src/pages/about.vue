6924ad671b506307069a42db681149bf7bfabb27
Thomas Boyt <me@thomasboyt.com>
2020-10-04 06:34:45 -0400

    App: Clean up about page content

--- app/src/pages/about.vue
+++ app/src/pages/about.vue
@@ -20,8 +20,8 @@
       <li>
         <p>
           <strong>
-            jam buds is dedicated to providing a harassment-free experience
-            for everyone.
+            jam buds is dedicated to providing a harassment-free experience for
+            everyone.
           </strong>
           I do not tolerate harassment of participants in any form.
         </p>
@@ -32,22 +32,18 @@
           if you are really excited to share a lot of new music you're into,
           that's okay, but if you're clogging up the feed by posting 27 cover
           versions of "who let the dogs out" at once, i will probably remove
-          your ability to post to the public feed at the very least. if you
-          want to share a whole lot of music at once, consider
-          <a
-            href="https://jambuds.club/mixtapes/29/who-did-it"
-          >a mixtape</a>
+          your ability to post to the public feed at the very least. if you want
+          to share a whole lot of music at once, consider
+          <a href="https://jambuds.club/mixtapes/29/who-did-it">a mixtape</a>
           instead!
         </p>
       </li>
     </ul>
 
     <p>
-      to report any violations of the above rules you have experienced, seen,
-      or heard about, please reach out to me at
-      <a
-        href="mailto:hello@jambuds.club"
-      >hello@jambuds.club</a>.
+      to report any violations of the above rules you have experienced, seen, or
+      heard about, please reach out to me at
+      <a href="mailto:hello@jambuds.club">hello@jambuds.club</a>.
     </p>
 
     <p>I reserve the right to remove any user from the site at any time.</p>
@@ -59,21 +55,20 @@
       on-fire-ness. you can check
       <a
         href="https://www.notion.so/thomasboyt/d4cf1e506401423094ff46ea977451b0?v=6f1d9cb6cfe44adabd4cd8f4f2072da9"
-      >the public issue tracker</a>
-      to see if your idea is on the drawing board, or if the bug you noticed
-      is already known. otherwise, feel free to reach out at
-      <a
-        href="mailto:hello@jambuds.club"
-      >hello@jambuds.club</a>!
+        >the public issue tracker</a
+      >
+      to see if your idea is on the drawing board, or if the bug you noticed is
+      already known. otherwise, feel free to reach out at
+      <a href="mailto:hello@jambuds.club">hello@jambuds.club</a>!
     </p>
 
     <h3>who's responsible for this mess</h3>
 
     <p>
       this site was built by
-      <a href="https://thomasboyt.com/">thomas</a>, who
-      feels weird writing this in the third person. you can
-      <nuxt-link to="/users/thomas">follow him on here</nuxt-link>if you'd
+      <a href="https://thomasboyt.com/">thomas</a>, who feels weird writing this
+      in the third person. you can
+      <nuxt-link to="/users/thomas">follow him on here</nuxt-link> if you'd
       like.
     </p>
 
@@ -102,16 +97,10 @@
 
     <p>
       the code for jam buds is open source and viewable
-      <a
-        href="https://github.com/thomasboyt/jam-buds"
-      >on github</a>. please
+      <a href="https://github.com/thomasboyt/jam-buds">on github</a>. please
       feel free to take a look, but we're not currently accepting outside
       contributions, so don't go opening any PRs!
     </p>
-
-    <!-- <p v-if="buildSha">
-        running client version {{buildSha}}
-    </p>-->
   </main-wrapper>
 </template>
 
@@ -124,12 +113,6 @@ export default {
   head() {
     return {
       title: 'About',
-    };
-  },
-
-  data() {
-    return {
-      // buildSha: process.env.BUILD_SHA,
     };
   },
 };
