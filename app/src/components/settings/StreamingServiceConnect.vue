727a40f9dbda5c615f96b592d2499577cdb0b419
Thomas Boyt <me@thomasboyt.com>
2020-10-05 11:38:45 -0400

    App: Add missing isDisconnecting state field

--- app/src/components/settings/StreamingServiceConnect.vue
+++ app/src/components/settings/StreamingServiceConnect.vue
@@ -40,6 +40,12 @@ export default {
     };
   },
 
+  data() {
+    return {
+      isDisconnecting: false,
+    };
+  },
+
   computed: {
     ...mapState({
       hasSpotify: (state) => state.streaming.hasSpotify,
