3718ff9d9e1ef6ef8aab67ebc10ab3a1950967e8
Thomas Boyt <me@thomasboyt.com>
2021-02-25 00:51:48 -0500

    API: Refactor likes endpoints to have /songs/id structure

--- rhiannon/src/main/kotlin/club/jambuds/service/LikeService.kt
+++ rhiannon/src/main/kotlin/club/jambuds/service/LikeService.kt
@@ -3,31 +3,40 @@ package club.jambuds.service
 import club.jambuds.dao.LikeDao
 import club.jambuds.dao.SongDao
 import club.jambuds.model.User
+import club.jambuds.util.ItemType
 import io.javalin.http.BadRequestResponse
 import io.javalin.http.NotFoundResponse
 
 class LikeService(private val likeDao: LikeDao, private val songDao: SongDao) {
-    fun createSongLike(currentUser: User, songId: Int) {
+    fun createLike(currentUser: User, itemType: ItemType, itemId: Int) {
         val userId = currentUser.id
 
-        if (!songDao.getSongExistsById(songId)) {
-            throw NotFoundResponse("Cannot find song with ID $songId")
-        }
+        if (itemType == ItemType.SONG) {
+            if (!songDao.getSongExistsById(itemId)) {
+                throw NotFoundResponse("Cannot find song with ID $itemId")
+            }
 
-        if (likeDao.getSongLikeExists(userId = userId, songId = songId)) {
-            throw BadRequestResponse("You have already a like for this song")
-        }
+            if (likeDao.getSongLikeExists(userId = userId, songId = itemId)) {
+                throw BadRequestResponse("You have already a like for this song")
+            }
 
-        likeDao.createSongLike(userId = userId, songId = songId)
+            likeDao.createSongLike(userId = userId, songId = itemId)
+        } else {
+            throw NotImplementedError()
+        }
     }
 
-    fun deleteSongLike(currentUser: User, songId: Int) {
+    fun deleteLike(currentUser: User, itemType: ItemType, itemId: Int) {
         val userId = currentUser.id
 
-        if (!likeDao.getSongLikeExists(userId = userId, songId = songId)) {
-            throw NotFoundResponse("You do not have a like for this song")
-        }
+        if (itemType == ItemType.SONG) {
+            if (!likeDao.getSongLikeExists(userId = userId, songId = itemId)) {
+                throw NotFoundResponse("You do not have a like for this song")
+            }
 
-        likeDao.deleteSongLike(userId = userId, songId = songId)
+            likeDao.deleteSongLike(userId = userId, songId = itemId)
+        } else {
+            throw NotImplementedError()
+        }
     }
 }
---
+++ rhiannon/src/main/kotlin/club/jambuds/util/ItemType.kt
@@ -0,0 +1,6 @@
+package club.jambuds.util
+
+enum class ItemType(val type: String) {
+    SONG("song"),
+    MIXTAPE("mixtape")
+}
