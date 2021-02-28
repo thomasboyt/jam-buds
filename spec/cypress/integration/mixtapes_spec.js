d20b7bd55e095201f6b885b3220c6d8c03de2abe
Thomas Boyt <me@thomasboyt.com>
2021-02-27 22:26:56 -0500

    Spec: Update mixtape integration tests

--- spec/cypress/integration/mixtapes_spec.js
+++ spec/cypress/integration/mixtapes_spec.js
@@ -1,25 +1,30 @@
 describe('creating mixtapes', () => {
   it('directs you to a new mixtape', () => {
     cy.login('jeff@jambuds.club');
-    cy.visit('/users/jeff/mixtapes');
+    cy.visit('/');
 
-    cy.get('button').contains('+ new mixtape').click();
+    cy.get('[data-test="post-jam-button"]:visible').click();
+    cy.get('button').contains('mixtapes').click();
+    cy.get('[data-test="new-jam-field"]').clear().type('jefftape');
+    cy.get('button[type="submit"]').click();
 
-    cy.url().should('match', /\/mixtapes\/(.*)\/new-mixtape$/);
+    cy.url().should('match', /\/mixtapes\/(.*)\/jefftape$/);
   });
 });
 
 describe('editing mixtapes', () => {
   beforeEach(() => {
     cy.login('jeff@jambuds.club');
-    cy.visit('/users/jeff/mixtapes');
-
-    cy.get('button').contains('+ new mixtape').click();
+    cy.visit('/');
+    cy.get('[data-test="post-jam-button"]:visible').click();
+    cy.get('button').contains('mixtapes').click();
+    cy.get('[data-test="new-jam-field"]').clear().type('jefftape');
+    cy.get('button[type="submit"]').click();
 
     cy.url()
-      .should('match', /\/mixtapes\/(.*)\/new-mixtape$/)
+      .should('match', /\/mixtapes\/(.*)\/jefftape$/)
       .then((url) => {
-        const id = url.match(/\/mixtapes\/(.*)\/new-mixtape$/)[1];
+        const id = url.match(/\/mixtapes\/(.*)\/jefftape$/)[1];
         return id;
       })
       .as('mixtapeId');
@@ -34,5 +39,29 @@ describe('editing mixtapes', () => {
 
     cy.url().should('include', `/mixtapes/${this.mixtapeId}/updated-title`);
     cy.contains('Updated Title');
+  });
+
+  it('adds new songs', () => {
+    cy.get('[data-test="post-jam-button"]:visible').click();
+
+    cy.get('[data-test="new-jam-field"]').clear().type('Twice');
+    cy.get('button[type="submit"]').click();
+    cy.contains('[data-test="search-results"] a', 'TT').click();
+    cy.get('[data-test="new-jam-confirm"]').click();
+
+    cy.contains('.playlist-item-row', 'TT').and('contain', 'TWICE');
+  });
+
+  it('allows publishing', () => {
+    cy.get('[data-test="post-jam-button"]:visible').click();
+    cy.get('[data-test="new-jam-field"]').clear().type('Twice');
+    cy.get('button[type="submit"]').click();
+    cy.contains('[data-test="search-results"] a', 'TT').click();
+    cy.get('[data-test="new-jam-confirm"]').click();
+    cy.contains('.playlist-item-row', 'TT').and('contain', 'TWICE');
+    cy.get('.publish-button').click();
+
+    cy.visit('/');
+    cy.get('.playlist-item-row').should('contain', 'jefftape');
   });
 });
