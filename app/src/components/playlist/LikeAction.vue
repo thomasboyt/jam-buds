1142e567b08d4380b3803606f1f4f223ebb6bf1f
Thomas Boyt <me@thomasboyt.com>
2021-04-16 02:00:58 -0400

    App: Fix liking a mixtape changing color scheme to mixtape author's

--- app/src/components/playlist/LikeAction.vue
+++ app/src/components/playlist/LikeAction.vue
@@ -49,6 +49,7 @@ export default {
   methods: {
     async handleToggleLike(e) {
       e.preventDefault();
+      e.stopPropagation();
 
       const action = this.isLiked ? 'unlikeItem' : 'likeItem';
 
